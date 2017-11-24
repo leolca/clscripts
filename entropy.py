cf3d5783d3364074c48b2497237844f3a13ac8df
leolca <leolca@gmail.com>
2017-11-24 20:18:50 -0200

    updated to read from file or stdin

--- entropy.py
+++ entropy.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import os
 import math 
+import argparse
 
-if len(sys.argv) > 1:
-  base = int(sys.argv[1])
+def is_valid_file(parser, arg):
+    arg = os.path.abspath(arg)
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return arg
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--base", help="Entropy base (default = 2, bits).", type=int, default=2)
+parser.add_argument("-i", dest="filename", help="Input text file.", metavar="FILE", type=lambda x: is_valid_file(parser, x))
+args = parser.parse_args()
+
+base = 2
+if args.base:
+  base = args.base
+
+if args.filename:
+  f = open(args.filename, "rt")
 else:
-  base = 2
+  f = sys.stdin
 
 x = []
-for line in sys.stdin:
+for line in f.readlines():
   x.append( float(line) )
 
 p = []
