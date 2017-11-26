7407c59848e6dd344268d8a2ad7745673c8cdcb4
leolca <leolca@gmail.com>
2017-11-26 11:14:02 -0200

    count occurrences from input file or stdin

--- entropy.py
+++ entropy.py
@@ -5,6 +5,14 @@ import os
 import math 
 import argparse
 
+
+# compute entropy from probabilities
+# p: counts
+# b: base
+def entropy(p, b):
+  p = filter(lambda a: a != 0, p)
+  return sum( [ -pp*math.log(pp,b) for pp in p ] )
+
 def is_valid_file(parser, arg):
     arg = os.path.abspath(arg)
     if not os.path.exists(arg):
@@ -15,6 +23,7 @@ def is_valid_file(parser, arg):
 parser = argparse.ArgumentParser()
 parser.add_argument("--base", help="Entropy base (default = 2, bits).", type=int, default=2)
 parser.add_argument("-i", dest="filename", help="Input text file.", metavar="FILE", type=lambda x: is_valid_file(parser, x))
+parser.add_argument("--method", help="Specify which method will be used to estimate the entropy.", default="mle", choices=['mle','plugin','jk','jackknife','mm','millermadow'])
 args = parser.parse_args()
 
 base = 2
@@ -26,19 +35,31 @@ if args.filename:
 else:
   f = sys.stdin
 
-x = []
+n = []
 for line in f.readlines():
   line = line.replace(',',' ')
   values = line.split()
   for v in values:
-      x.append( float(v) )
+      n.append( float(v) )
 
-p = []
-M = sum(x)
-if M != 1:
-  p = [xx/M for xx in x] 
-else:
-  p = x
 
-H = sum( [ -pp*math.log(pp,base) for pp in p ] )
+M = sum(n)    # sample size
+N = len(n) # vocabulary size
+if args.method == 'mle' or args.method == 'plugin':
+  p = [nn/M for nn in n]
+  #H = sum( [ -pp*math.log(pp,base) for pp in p ] )
+  H = entropy(p, base)
+elif args.method == 'jk' or args.method == 'jackknife':
+  n = filter(lambda a: a != 0, n)
+  N = len(n)
+  p = [nn/M for nn in n]
+  #H = M*sum( [ -pp*math.log(pp,base) for pp in p ] )
+  H = M*entropy(p, base)
+  for i in range(0,N):
+     nt = n
+     nt[i] -= 1
+     Mt = sum(nt)
+     p = [nn/Mt for nn in nt]
+     H -= ((M-1)/M) * n[i] * entropy(p,base)
+   
 print H
--- wordcounttfl.sh
+++ wordcounttfl.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo -e "f\ttype" 
-tr "A-Z" "a-z" < $1 | tr -sc "A-Za-z\'" "\n" | sed -e "s/'$//" | sed -e "s/^'//" | sort | uniq -c | sort -n -r | sed 's/[[:space:]]*\([0-9]*\) \([a-z]*\)/\1\t\2/'
+if [ "$1" ]; then 
+   cat "$1"
+else 
+   cat
+fi | tr "A-Z" "a-z" | tr -sc "A-Za-z\'" "\n" | sed -e "s/'$//" | sed -e "s/^'//" | sort | uniq -c | sort -n -r | sed 's/[[:space:]]*\([0-9]*\) \([a-z]*\)/\1\t\2/'
